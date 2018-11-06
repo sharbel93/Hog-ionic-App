c6e4e537fae2dc4d743cc27884bd4d1e16712112
sharbel chris <sharbel.chris@gmail.com>
2018-11-06 14:50:29 +0300

    adding splash animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+//we import an RxJS timer that will toggle the visibility of the overlay after 3000ms.
+import { timer } from 'rxjs/observable/timer';
+
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -9,13 +12,14 @@ import { HomePage } from '../pages/home/home';
 })
 export class MyApp {
   rootPage:any = HomePage;
-
+  showSplash = true; // show animation
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();
+      splashScreen.hide(); // hide static image
+      timer(3000).subscribe(() => this.showSplash = false); // hide animation after 3s
     });
   }
 }
